e36fbcf33454993e1b48db3556c15d2526203a9d
myOmikron <git@omikron.dev>
2023-03-24 00:16:43 +0100

    expanded vagrant

--- borg-vinculum/src/config.rs
+++ borg-vinculum/src/config.rs
@@ -1,7 +1,58 @@
 //! The configuration of `borg-vinculum`
 
+use std::fs::read_to_string;
+use std::net::IpAddr;
+
+use actix_toolbox::logging::LoggingConfig;
 use serde::{Deserialize, Serialize};
+
+/// Configuration regarding the server
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct ServerConfig {
+    /// The address the server should bind to
+    pub listen_address: IpAddr,
+    /// The port the server should bind to
+    pub listen_port: u16,
+    /// Base64 encoded secret key
+    ///
+    /// The key is used to sign and verify sessions.
+    ///
+    /// Do not expose this key!
+    pub secret_key: String,
+}
+
+/// Configuration regarding the database
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct DBConfig {
+    /// Host the database is located on
+    pub host: String,
+    /// Port the database is located on
+    pub port: u16,
+    /// The name of the database to connect to.
+    pub name: String,
+    /// The username to use for the database connection
+    pub user: String,
+    /// The password to use for the database connection
+    pub password: String,
+}
 
 /// The configuration file of borg-vinculum
 #[derive(Serialize, Deserialize)]
-pub struct Config {}
+pub struct Config {
+    /// Configuration regarding the server
+    pub server: ServerConfig,
+    /// The logging configuration
+    pub logging: LoggingConfig,
+    /// The database configuration
+    pub database: DBConfig,
+}
+
+impl Config {
+    /// Retrieve a config file using the `path` variable.
+    pub fn from_path(path: &str) -> Result<Config, String> {
+        let config_str = read_to_string(path).map_err(|e| e.to_string())?;
+        toml::from_str(&config_str).map_err(|e| e.to_string())
+    }
+}
---
+++ borg-vinculum/src/server.rs
@@ -0,0 +1,8 @@
+//! The server is defined in this module
+
+use crate::config::Config;
+
+/// Start the server
+pub async fn start_server(config: &Config) -> Result<(), String> {
+    Ok(())
+}
